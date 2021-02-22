a10c6ed128d23e72d1fbbeb05f12c57e24f12fdc
mpeace <mpeace@oakland.edu>
2021-02-22 15:59:14 -0500

    Model changes

--- src/webapp/models.py
+++ src/webapp/models.py
@@ -250,6 +250,8 @@ class Foundation(models.Model):
     settlement = models.TextField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
     improvements_id = models.ForeignKey(DescriptionOfImprovements, models.DO_NOTHING)
+=======
+>>>>>>> Model changes
     
 class House(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -258,9 +260,8 @@ class House(models.Model):
     state = models.TextField()
     zip = models.TextField()
     county = models.TextField()
-    appraisal_status = models.TextField()  # This field type is a guess.
-    appraiser = models.ForeignKey(AuthUser, models.DO_NOTHING, blank=True, null=True)
-    customer = models.ForeignKey(AuthUser, models.DO_NOTHING, blank=True, null=True)
+    appraisal_status = models.TextField()
+    appraiser = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
     
 class HouseFeatures(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -274,18 +275,26 @@ class HouseFeatures(models.Model):
     attic = models.TextField()
     comments = models.TextField()
     house = models.ForeignKey(House, models.DO_NOTHING)
-
-class Kitchen(models.Model):
+    
+class DescriptionOfImprovements(models.Model):
     id = models.IntegerField(primary_key=True)
-    kitchen_refrig = models.BooleanField()
-    kitchen_oven = models.BooleanField()
-    kitchen_disposal = models.BooleanField()
-    kitchen_dishwasher = models.BooleanField()
-    kitchen_fan_hood = models.BooleanField()
-    kitchen_microwave = models.BooleanField()
-    kitchen_description = models.TextField(blank=True, null=True)
+    design_style = models.TextField(blank=True, null=True)
+    age = models.IntegerField()
+    effective_age = models.IntegerField(blank=True, null=True)
+    walls = models.TextField(blank=True, null=True)
+    roof_surface = models.TextField(blank=True, null=True)
+    gutters_downspouts = models.TextField(blank=True, null=True)
+    win_type = models.TextField(blank=True, null=True)
+    storm_screens = models.TextField(blank=True, null=True)
+    roof_insulation = models.BooleanField()
+    ceiling_insulation = models.BooleanField()
+    walls_insulation = models.BooleanField()
+    floor_insulation = models.BooleanField()
+    none_insulation = models.BooleanField()
+    unknown_insulation = models.BooleanField()
     comments = models.TextField(blank=True, null=True)
-    materials_condition = models.ForeignKey('MaterialsAndCondition', models.DO_NOTHING)
+    house = models.ForeignKey('House', models.DO_NOTHING)
+    
     
 class MaterialsAndCondition(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -303,36 +312,48 @@ class MaterialsAndCondition(models.Model):
     materials_doors = models.TextField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
     house = models.ForeignKey(House, models.DO_NOTHING)
-    
-class Neighborhood(models.Model):
+
+class Kitchen(models.Model):
     id = models.IntegerField(primary_key=True)
-    location = models.TextField()  # This field type is a guess.
-    name = models.IntegerField()
-    built_up = models.TextField()  # This field type is a guess.
-    growth_rate = models.TextField()  # This field type is a guess.
-    property_value = models.TextField()  # This field type is a guess.
-    demand_supply = models.TextField()  # This field type is a guess.
-    marketability_factors = models.TextField()
-    market_conditions = models.TextField()
+    kitchen_refrig = models.BooleanField()
+    kitchen_oven = models.BooleanField()
+    kitchen_disposal = models.BooleanField()
+    kitchen_dishwasher = models.BooleanField()
+    kitchen_fan_hood = models.BooleanField()
+    kitchen_microwave = models.BooleanField()
+    kitchen_description = models.TextField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
     house = models.ForeignKey(House, models.DO_NOTHING)
 
  
 class Offsite(models.Model):
-    id = models.IntegerField(primary_key=True)
-    offsite_curb_note = models.TextField(blank=True, null=True)
-    offsite_street_note = models.TextField(blank=True, null=True)
-    offsite_sidewalk_note = models.TextField(blank=True, null=True)
-    offsite_streetlight_note = models.TextField(blank=True, null=True)
-    offsite_alley_note = models.TextField(blank=True, null=True)
-    offsite_streetlights = models.TextField()  # This field type is a guess.
-    offsite_curb = models.TextField()  # This field type is a guess.
-    offsite_sidewalk = models.TextField()  # This field type is a guess.
-    offsite_alley = models.TextField()  # This field type is a guess.
-    offsite_street = models.TextField()  # This field type is a guess.
-    comments = models.TextField(blank=True, null=True)
-    site = models.ForeignKey('Site', models.DO_NOTHING)
+    materials_condition = models.ForeignKey('MaterialsAndCondition', models.DO_NOTHING)
     
+    
+class Foundation(models.Model):
+    id = models.IntegerField(primary_key=True)
+    sump_pump = models.BooleanField()
+    foundation_ext = models.TextField(blank=True, null=True)
+    slab = models.BooleanField()
+    crawl_space = models.BooleanField()
+    dampness = models.TextField(blank=True, null=True)
+    infestation = models.TextField(blank=True, null=True)
+    settlement = models.TextField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+    improvements = models.ForeignKey(DescriptionOfImprovements, models.DO_NOTHING)
+    
+class Basement(models.Model):
+    id = models.IntegerField(primary_key=True)
+    basement_area = models.IntegerField(blank=True, null=True)
+    basement_percent_finished = models.TextField(blank=True, null=True)
+    basement_ceiling = models.TextField(blank=True, null=True)
+    basement_walls = models.TextField(blank=True, null=True)
+    basement_floor = models.TextField(blank=True, null=True)
+    basement_outside_entry = models.TextField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+    materials_conditions = models.ForeignKey('MaterialsAndCondition', models.DO_NOTHING)
+
+
 class Property(models.Model):
     id = models.IntegerField(primary_key=True)
     borrower = models.TextField()
@@ -348,8 +369,8 @@ class Property(models.Model):
     sale_price = models.IntegerField(blank=True, null=True)
     date_of_sale = models.DateField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
-    house_id = models.ForeignKey(House, models.DO_NOTHING)
-
+    house = models.ForeignKey(House, models.DO_NOTHING)
+    
 class Room(models.Model):
     id = models.IntegerField(primary_key=True)
     room_type = models.TextField(blank=True, null=True)  # This field type is a guess.
@@ -357,7 +378,7 @@ class Room(models.Model):
     room_area = models.IntegerField(blank=True, null=True)
     room_comments = models.TextField(blank=True, null=True)
     house = models.ForeignKey(House, models.DO_NOTHING)
-
+    
 class Site(models.Model):
     id = models.IntegerField(primary_key=True)
     dimensions = models.TextField(blank=True, null=True)
@@ -384,7 +405,22 @@ class Site(models.Model):
     map_date = models.DateField(blank=True, null=True)
     comments = models.TextField(blank=True, null=True)
     house = models.ForeignKey(House, models.DO_NOTHING)
- 
+    
+class Offsite(models.Model):
+    id = models.IntegerField(primary_key=True)
+    offsite_curb_note = models.TextField(blank=True, null=True)
+    offsite_street_note = models.TextField(blank=True, null=True)
+    offsite_sidewalk_note = models.TextField(blank=True, null=True)
+    offsite_streetlight_note = models.TextField(blank=True, null=True)
+    offsite_alley_note = models.TextField(blank=True, null=True)
+    offsite_streetlights = models.TextField()  # This field type is a guess.
+    offsite_curb = models.TextField()  # This field type is a guess.
+    offsite_sidewalk = models.TextField()  # This field type is a guess.
+    offsite_alley = models.TextField()  # This field type is a guess.
+    offsite_street = models.TextField()  # This field type is a guess.
+    comments = models.TextField(blank=True, null=True)
+    site = models.ForeignKey('Site', models.DO_NOTHING)
+
 class Utilities(models.Model):
     id = models.IntegerField(primary_key=True)
     heat_type = models.TextField()
@@ -396,7 +432,7 @@ class Utilities(models.Model):
     comments = models.TextField()
     materials_conditions = models.ForeignKey(MaterialsAndCondition, models.DO_NOTHING)
     
-class WebappAppraisal(models.Model):
+class Appraisal(models.Model):
     id = models.IntegerField(primary_key=True)
     house = models.ForeignKey(House, models.DO_NOTHING)
     positive_features = models.TextField(blank=True, null=True)
