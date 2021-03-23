a2600b0cbef428393a66cb24c1bd88e9f4ea55d0
AZX309 <74437643+AZX309@users.noreply.github.com>
2021-03-23 17:40:39 -0400

    Implmiented basement,foundation,kitchen,offsite,site, and utilities customer views

--- src/webapp/view/basement.py
+++ src/webapp/view/basement.py
@@ -9,21 +9,50 @@ from src.webapp.models import *
 
 @login_required(login_url='/welcome')
 def view(request, house_id):
-    house_instance = House.objects.filter(id=house_id).first()
+    # TODO: Add generic error page to redirect to when don't have access
     # assert hasAccessToAppraisal(user_id=request.user.id, house_id=house_id) is True
     role = Profile.objects.get(user_id=request.user.id).role
+    images = Image.objects.filter(house_id=house_id, page=Image.Pages.BASEMENT)
+
     if role == Profile.Roles.APPRAISER:
+        is_mobile = request.user_agent.is_mobile
+        add_image_form = NewImageForm(request.POST)
+        mobile_img_form = MobileImageForm(request.POST)
+
         if request.method == 'POST':
             # shared logic among views for user logout
             if 'user_logout' in request.POST:
                 logout(request)
                 redirect('/welcome')
 
+            elif 'img' in request.FILES:
+                form = NewImageForm(request.POST, request.FILES)
+                if form.is_valid():
+                    new_img = form.save(commit=False)
+                    # set page
+                    new_img.page = Image.Pages.BASEMENT
+                    # set house id
+                    new_img.house = House.objects.filter(id=house_id).first()
+                    new_img.save()
+                    return redirect('/basement/%s/' % house_id)
+                else:
+                    return redirect('/basement/%s/' % house_id)
+
+            elif 'submit_desc' in request.POST:
+                img_id = request.POST['img_id']
+                img_instance = Image.objects.get(id=img_id)
+                form = ImageFormWithDescription(request.POST, instance=img_instance)
+                if form.is_valid():
+                    form.save()
+                    return redirect('/basement/%s/' % house_id)
+                else:
+                    return redirect('/basement/%s/' % house_id)
+
             # on the button: <input type=submit name=update_account
             if 'submit_basement_info' in request.POST:
                 # we need to update the object
-                if Basement.objects.filter(house=house_instance).exists():
-                    basement_info = Basement.objects.get(house=house_instance)
+                if Basement.objects.filter(house_id=house_id).exists():
+                    basement_info = Basement.objects.get(house_id=house_id)
                     form = BasementForm(request.POST, instance=basement_info)
 
                     if form.is_valid():
@@ -54,14 +83,18 @@ def view(request, house_id):
 
         # haven't submitted anything - get blank form if object doesn't exist or create form using existing object
         else:
-            if Basement.objects.filter(house=house_instance).exists():
-                basement_info = Basement.objects.get(house=house_instance)
+            img_forms = list(map(lambda img: ImageFormWithDescription(instance=img), images))
+
+            if Basement.objects.filter(house=house_id).exists():
+                basement_info = Basement.objects.get(house=house_id)
                 form = BasementForm(instance=basement_info)
             else:
                 form = BasementForm(request.POST)
 
             return render(request, 'appraisal_edit_forms/basement.html',
-                          context={'form': form, 'house_id': house_id})
+                          context={'form': form, 'house_id': house_id,
+                                   'is_mobile': is_mobile, 'mobile_img_form': mobile_img_form,
+                                   'img_forms': img_forms, 'new_img_form': add_image_form})
     else:
         if Basement.objects.filter(house_id=house_id).exists():
             basement_info = Basement.objects.get(house_id=house_id)
--- src/webapp/view/kitchen.py
+++ src/webapp/view/kitchen.py
@@ -9,21 +9,50 @@ from src.webapp.models import *
 
 @login_required(login_url='/welcome')
 def view(request, house_id):
-    house_instance = House.objects.filter(id=house_id).first()
+    # TODO: Add generic error page to redirect to when don't have access
     # assert hasAccessToAppraisal(user_id=request.user.id, house_id=house_id) is True
     role = Profile.objects.get(user_id=request.user.id).role
+    images = Image.objects.filter(house_id=house_id, page=Image.Pages.KITCHEN)
+
     if role == Profile.Roles.APPRAISER:
+        is_mobile = request.user_agent.is_mobile
+        add_image_form = NewImageForm(request.POST)
+        mobile_img_form = MobileImageForm(request.POST)
+
         if request.method == 'POST':
             # shared logic among views for user logout
             if 'user_logout' in request.POST:
                 logout(request)
                 redirect('/welcome')
 
+            elif 'img' in request.FILES:
+                form = NewImageForm(request.POST, request.FILES)
+                if form.is_valid():
+                    new_img = form.save(commit=False)
+                    # set page
+                    new_img.page = Image.Pages.KITCHEN
+                    # set house id
+                    new_img.house = House.objects.filter(id=house_id).first()
+                    new_img.save()
+                    return redirect('/kitchen/%s/' % house_id)
+                else:
+                    return redirect('/kitchen/%s/' % house_id)
+
+            elif 'submit_desc' in request.POST:
+                img_id = request.POST['img_id']
+                img_instance = Image.objects.get(id=img_id)
+                form = ImageFormWithDescription(request.POST, instance=img_instance)
+                if form.is_valid():
+                    form.save()
+                    return redirect('/kitchen/%s/' % house_id)
+                else:
+                    return redirect('/kitchen/%s/' % house_id)
+
             # on the button: <input type=submit name=update_account
             if 'submit_kitchen_info' in request.POST:
                 # we need to update the object
-                if Kitchen.objects.filter(house=house_instance).exists():
-                    kitchen_info = Kitchen.objects.get(house=house_instance)
+                if Kitchen.objects.filter(house_id=house_id).exists():
+                    kitchen_info = Kitchen.objects.get(house_id=house_id)
                     form = KitchenForm(request.POST, instance=kitchen_info)
 
                     if form.is_valid():
@@ -54,17 +83,21 @@ def view(request, house_id):
 
         # haven't submitted anything - get blank form if object doesn't exist or create form using existing object
         else:
-            if Kitchen.objects.filter(house=house_instance).exists():
-                kitchen_info = Kitchen.objects.get(house=house_instance)
+            img_forms = list(map(lambda img: ImageFormWithDescription(instance=img), images))
+
+            if Kitchen.objects.filter(house=house_id).exists():
+                kitchen_info = Kitchen.objects.get(house=house_id)
                 form = KitchenForm(instance=kitchen_info)
             else:
                 form = KitchenForm(request.POST)
 
             return render(request, 'appraisal_edit_forms/kitchen.html',
-                          context={'form': form, 'house_id': house_id})
+                          context={'form': form, 'house_id': house_id,
+                                   'is_mobile': is_mobile, 'mobile_img_form': mobile_img_form,
+                                   'img_forms': img_forms, 'new_img_form': add_image_form})
     else:
-        if Kitchen.objects.filter(house=house_id).exists():
-            kitchen_info = Kitchen.objects.get(house=house_id)
+        if Kitchen.objects.filter(house_id=house_id).exists():
+            kitchen_info = Kitchen.objects.get(house_id=house_id)
         else:
             kitchen_info = 'empty'
         return render(request, 'customer_view_forms/view_kitchen.html',
